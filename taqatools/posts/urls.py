c6ca6587fc7c5dd4d466c450eb9833640fa56d2d
Osama <osama.nasr.01@gmail.com>
2023-08-27 20:07:20 +0300

    adding posts and add post form

--- taqatools/posts/urls.py
+++ taqatools/posts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('posts/', views.posts, name='posts'),
+    path('posts/add-post', views.add_post, name='add_post')
 ]
--- taqatools/posts/views.py
+++ taqatools/posts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from products.models import Category
+from .models import Post
+import json
 
 # Create your views here.
+def posts(request):
+    return render(request, 'posts/posts.html', {})
+
+
+
+def add_post(request):
+    if request.method == 'POST':
+        print('post')
+        data = json.loads(request.body)
+        print(data)
+        new_post = Post.objects.create(
+            title = data['title'],
+            content = data['content'],
+            category = data['category']
+        )
+        new_post.save
+        json_data = json.dumps({'ok':'ok'})
+        return HttpResponse(json_data, content_type="application/json")
+    else:
+        categories  = Category.objects.all()
+        return render(request, 'posts/add_post.html', {'categories': categories})
