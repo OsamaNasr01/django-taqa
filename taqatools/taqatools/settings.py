3928791df7bdce2614203e2c89c6a1e867e17582
Osama <osama.nasr.01@gmail.com>
2023-11-16 19:27:59 +0200

    change setting file to be combatable with ubuntu

--- taqatools/taqatools/settings.py
+++ taqatools/taqatools/settings.py
@@ -130,15 +130,15 @@ USE_TZ = True
 
 MEDIA_ROOT = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'members/static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/members/static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'members\static\members', 'bootstrap'),
+    os.path.join(BASE_DIR, 'members/static/members', 'bootstrap'),
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CKEDITOR_UPLOAD_PATH = "uploads/"
+CKEDITOR_UPLOAD_PATH = "/uploads/"
 
