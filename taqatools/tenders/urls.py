5e7fa7c43f542ce59be6024750430ebad79962e1
Osama <osama.nasr.01@gmail.com>
2023-12-10 16:20:06 +0200

    adding update, delete and list for tender

--- taqatools/tenders/urls.py
+++ taqatools/tenders/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('tenders/add/', views.add_tender, name = 'add_tender'),
     path('tender/<int:id>/', views.tender_profile, name = 'tender_profile'),
+    path('tender/<int:id>/update/', views.tender_update, name = 'tender_update'),
+    path('tender/<int:id>/delete/', views.tender_delete, name = 'tender_delete'),
+    path('tenders/list/', views.tenders_list, name = 'tenders_list'),
 
 ]
--- taqatools/tenders/views.py
+++ taqatools/tenders/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Tender
 from django.contrib import messages
+from members.views import home
 
 # Create your views here.
 def add_tender(request):
@@ -21,3 +22,30 @@ def tender_profile(request, id):
     return render(request, 'tenders/tender_profile.html', {
         'tender': tender,
     })
+
+
+
+def tender_update(request, id):
+    tender = Tender.objects.get(id = id)
+    if request.method == 'POST':
+        tender.name = request.POST['name']
+        tender.description = request.POST['description']
+        tender.image = request.FILES['image']
+        tender.save()
+        messages.success(request, 'تم تعديل بيانات المناقصة بنجاح')
+        return tender_profile(request, tender.id)
+    else:
+        return render(request, 'tenders/tender_update.html', {'tender':tender})
+    
+    
+    
+def tender_delete(request, id):
+    tender = Tender.objects.get(id = id)
+    tender.delete()
+    messages.success(request, 'تم حذف المناقصة بنجاح')
+    return home(request)
+    
+    
+def tenders_list(request):
+    tenders = Tender.objects.all()
+    return render(request, 'tenders/tenders_list.html', {'tenders':tenders})
